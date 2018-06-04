8acb8b2b4823e9011ebc2a27951a4bf48b9d44ce
Dhrumil Shah <dhuma1981@gmail.com>
2018-06-04 15:09:45 +0530

    ADD Column widget code

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,15 @@ void main() => runApp(
 class ColumnWidgetDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Container();
+    return  Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: <Widget>[
+        Text("1. Text", style: TextStyle(fontSize: 20.0),),
+        Text("2. Text", style: TextStyle(fontSize: 60.0),),
+        Text("3. Text", style: TextStyle(fontSize: 20.0),),
+      ],
+    );
   }
 }
 
