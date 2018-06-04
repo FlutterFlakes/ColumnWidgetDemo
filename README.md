db9558d1b2beab6c83c89addcbe762c621ef378e
Dhrumil Shah <dhuma1981@gmail.com>
2018-06-04 15:03:25 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# column_widget_demo
+
+Column Widget Demo
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+  MaterialApp(
+    title: "Column Demo",
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Column Demo"),
+      ),
+      body: ColumnWidgetDemo(),
+    ),
+  )
+);
+
+class ColumnWidgetDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Container();
+  }
+}
+
+
